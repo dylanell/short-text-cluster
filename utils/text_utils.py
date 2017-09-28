93ed401ab7de0736cb19cb1cad2a7dddb50d5656
dylanell <dellio15@gmail.com>
2017-09-28 15:18:36 -0400

    separate methods for building dictionary and bag of words

--- utils/text_utils.py
+++ utils/text_utils.py
@@ -11,14 +11,12 @@ import numpy as np
 import gensim
 
 def buildDict(documents, stoplist, stem=False):
-    print(stoplist)
-
+    # filter text using the stoplist
     texts = [[word for word in document.lower().split() if word not in stoplist]
             for document in documents]
 
-    print(texts)
-
     # remove words that appear once
+    """
     from collections import defaultdict
     frequency = defaultdict(int)
     for text in texts:
@@ -27,5 +25,31 @@ def buildDict(documents, stoplist, stem=False):
 
     texts = [[token for token in text if frequency[token] > 1]
             for text in texts]
+    """
 
-    print texts
+    # build dictionary
+    dictionary = gensim.corpora.Dictionary(texts)
+
+    return texts, dictionary
+
+# log normalized bag-of-words
+def docs2Bow(texts, dictionary):
+    # initialize bag of words array
+    n = len(texts)
+    d = len(dictionary)
+    X = np.zeros((n, d))
+
+    corpus = [dictionary.doc2bow(text) for text in texts]
+
+    for i in range(n):
+        for idx, val in corpus[i]:
+            X[i, idx] = val
+
+    # log normalize the vectors
+    for i in range(n):
+        x = X[None, i, :]
+        x = np.log(1 + x)
+        x = x/np.max(x)
+        X[i, :] = x
+    
+    return X
